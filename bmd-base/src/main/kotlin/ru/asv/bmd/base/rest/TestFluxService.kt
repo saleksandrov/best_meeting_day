4da3cd9d298047fb303ebf1e90a103866d11f213
17540258 <hk123890!>
2019-10-28 16:38:05 +0300

    Added WebFlux

---
+++ bmd-base/src/main/kotlin/ru/asv/bmd/base/rest/TestFluxService.kt
@@ -0,0 +1,18 @@
+package ru.asv.bmd.base.rest
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+import reactor.core.publisher.Mono
+
+
+@RestController
+@RequestMapping("/test")
+class TestFluxService {
+
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = ["/get"], method = [RequestMethod.GET])
+    fun test(): Mono<String> {
+        return Mono.just("TEST")
+    }
+
+}
