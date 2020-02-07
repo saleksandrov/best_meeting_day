9f1650e5dafb7490ed5fb75a2432266bcb69b2b5
17540258 <hk123890!>
2020-02-07 18:11:45 +0300

    Added multi dates

--- bmd-base/src/main/resources/ui/src/component/AddVote.jsx
+++ bmd-base/src/main/resources/ui/src/component/AddVote.jsx
@@ -1,10 +1,27 @@
 import React, {Component} from 'react';
+import MultipleDatesPicker from '@randex/material-ui-multiple-dates-picker'
+import {Button} from '@material-ui/core';
+
+const [open, setOpen] = useState(false)
 
 class AddVote extends Component {
 
+    constructor(props) {
+        super(props);
+        //const [open, setOpen] = useState(false)
+    }
+
     render() {
         return (
-            <div>Test</div>
+            <div>
+                <Button onClick={() => setOpen(!open)}>
+                    Select Dates
+                </Button>
+                <MultipleDatesPicker
+                    open="true"
+                    selectedDates={[]}
+                    onSubmit={dates => console.log('selected dates', dates)} />
+                    </div>
         );
     }
 }
