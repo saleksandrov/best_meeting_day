d9232050518c539579bd032710bf1785ee9a72a9
Sergey Aleksandrov <saleksandrov@gmail.com>
2020-02-26 16:57:49 +0300

    UI minor changes

--- bmd-base/src/main/resources/ui/src/component/AddVote.jsx
+++ bmd-base/src/main/resources/ui/src/component/AddVote.jsx
@@ -2,26 +2,33 @@ import React, {Component} from 'react';
 import MultipleDatesPicker from '@randex/material-ui-multiple-dates-picker'
 import {Button} from '@material-ui/core';
 
-const [open, setOpen] = useState(false)
-
 class AddVote extends Component {
 
     constructor(props) {
         super(props);
-        //const [open, setOpen] = useState(false)
+        this.state = {
+            open_flag: false
+        };
+
+        this.setOpen = this.setOpen.bind(this);
+    }
+
+    setOpen() {
+        this.state.open_flag = !this.state.open_flag;
     }
 
     render() {
         return (
             <div>
-                <Button onClick={() => setOpen(!open)}>
-                    Select Dates
+                <Button onClick={this.setOpen()}  name="selectdates" variant="contained" color="primary">
+                    Выберите даты
                 </Button>
                 <MultipleDatesPicker
-                    open="true"
+
+                    open={this.state.open_flag}
                     selectedDates={[]}
                     onSubmit={dates => console.log('selected dates', dates)} />
-                    </div>
+            </div>
         );
     }
 }
