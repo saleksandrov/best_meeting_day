83c5a9130d40457b18177bbd341902e4afb30b51
Sergey Aleksandrov <saleksandrov@gmail.com>
2020-04-13 22:19:30 +0300

    Externalized configration

---
+++ bmd-base/src/main/resources/ui/build/precache-manifest.9de0f88a2a40762f1739fe742436cb97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc5cc5b336246530aa33d7186574326",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebeebaabf978672a7f6e",
+    "url": "/static/css/2.3c81c441.chunk.css"
+  },
+  {
+    "revision": "5bb85d41888587989ee9",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ebeebaabf978672a7f6e",
+    "url": "/static/js/2.bf37c8a3.chunk.js"
+  },
+  {
+    "revision": "ad0d7d89397809cf7a3b0179e1fd23f4",
+    "url": "/static/js/2.bf37c8a3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5bb85d41888587989ee9",
+    "url": "/static/js/main.2c401dbe.chunk.js"
+  },
+  {
+    "revision": "e4ae35f2ceddadd1e866",
+    "url": "/static/js/runtime-main.a56637b1.js"
+  }
+]);
